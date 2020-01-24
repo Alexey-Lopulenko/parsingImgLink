999d88fc0d96102812bf8bfe131ca0ab26eb98db
url <urlskylogic@gmail.com>
2020-01-24 13:06:56 +0200

    first commit

---
+++ classes/parsing.php
@@ -0,0 +1,213 @@
+<?php
+
+class parsing
+{
+    public $currentUrl;
+    public $parseUrlLimit = 500;
+    public $internalLinks = [];
+    public $visitedPages = [];
+    public $recordedLinks;
+
+    public function __construct()
+    {
+        date_default_timezone_set('Europe/Kiev');
+    }
+
+    /**
+     * @param $url
+     * @return mixed
+     */
+    public function validateUrl($url)
+    {
+        if (get_headers($url, 1)) {
+            return $url;
+        } else {
+            if (get_headers('https://' . $url, 1)) {
+                $this->setCurrentUrl('https://' . $url);
+                return $this->getCurrentUrl();
+            } elseif (get_headers('http://' . $url, 1)) {
+                $this->setCurrentUrl('http://' . $url);
+                return $this->getCurrentUrl();
+            }
+        }
+    }
+
+
+    /**
+     * @param $link
+     * @return bool
+     */
+    public function mainLink($link)
+    {
+        $pos = strpos($link, '://' . $this->getDomain());
+        if ($pos === false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+    /**
+     * @param $link
+     */
+    public function deleteInternalLinks($link)
+    {
+        $arrInternalLinks = $this->getInternalLinks();
+        if (in_array($link, $arrInternalLinks)) {
+            unset($arrInternalLinks[array_search($link, $arrInternalLinks)]);
+        }
+        $this->internalLinks = [];
+
+        foreach ($arrInternalLinks as $item) {
+            array_push($this->internalLinks, $item);
+        }
+    }
+
+    public function addInternalLink($link)
+    {
+        if (!(in_array($link, $this->getInternalLinks())) && !(in_array($link, $this->getVisitedPages()))) {
+            array_push($this->internalLinks, $link);
+        }
+    }
+
+    public function setInternalLinks()
+    {
+        foreach ($this->getAllLinksFromThePage() as $link) {
+            if ($this->mainLink($link)) {
+                array_push($this->internalLinks, $link);
+            }
+        }
+    }
+
+    public function getInternalLinks()
+    {
+        return $this->internalLinks;
+    }
+
+    public function getAllLinksFromThePage()
+    {
+        $dom = new DOMDocument();
+        @$dom->loadHTML($this->getDataFromSite());
+        $allLinks = [];
+        $xpath = new DOMXPath($dom);
+        $hrefs = $xpath->evaluate("/html/body//a");
+
+        for ($i = 0; $i < $hrefs->length; $i++) {
+            $href = $hrefs->item($i);
+            $urls = $href->getAttribute('href');
+            array_push($allLinks, $urls . PHP_EOL);
+        }
+        return $allLinks;
+    }
+
+    /**
+     * @return string|string[]|null
+     */
+    public function getDomain()
+    {
+        $url = trim($this->getCurrentUrl(), '/');
+        if (!preg_match('#^http(s)?://#', $url)) {
+            $url = 'http://' . $url;
+        }
+        $urlParts = parse_url($url);
+        $domain = preg_replace('/^www\./', '', $urlParts['host']);
+        return $domain;
+    }
+
+
+    /**
+     * @param int $parseUrlLimit
+     */
+    public function setParseUrlLimit($parseUrlLimit)
+    {
+        $this->parseUrlLimit = $parseUrlLimit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParseUrlLimit()
+    {
+        return $this->parseUrlLimit;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getImageLink()
+    {
+        $file = "image.csv";
+        $imagesLink = [];
+
+        file_put_contents($file, "\n" . $this->getCurrentUrl() . "\n", FILE_APPEND | LOCK_EX);
+
+        preg_match_all('/(img|src)=("|\')[^"\'>]+/i', $this->getDataFromSite(), $media);
+        unset($data);
+        $data = preg_replace('/(img|src)("|\'|="|=\')(.*)/i', "$3", $media[0]);
+
+        foreach ($data as $url) {
+            $info = pathinfo($url);
+            if (isset($info['extension'])) {
+                if (($info['extension'] == 'jpg') ||
+                    ($info['extension'] == 'jpeg') ||
+                    ($info['extension'] == 'gif') ||
+                    ($info['extension'] == 'png')){
+                    array_push($imagesLink, $url);
+                }
+            }
+        }
+        foreach ($imagesLink as $value) {
+            file_put_contents($file, $value . "\n", FILE_APPEND | LOCK_EX);
+            array_push($this->recordedLinks, $value);
+        }
+    }
+
+    /**
+     * @param $url
+     */
+    public function setCurrentUrl($url)
+    {
+        $this->currentUrl = $this->validateUrl($url);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentUrl()
+    {
+        return $this->currentUrl;
+    }
+
+
+    /**
+     * @return false|string
+     */
+    public function getDataFromSite()
+    {
+        if ($this->getCurrentUrl()) {
+            $data = file_get_contents($this->getCurrentUrl());
+            return $data;
+        }
+        return 'Error set CurrentUrl!';
+    }
+
+    /**
+     * @param $visitedPage
+     */
+    public function setVisitedPages($visitedPage)
+    {
+        array_push($this->visitedPages, $visitedPage);
+    }
+
+    /**
+     * @return array
+     */
+    public function getVisitedPages()
+    {
+        return $this->visitedPages;
+    }
+
+}
--- cnsParing.php
+++ cnsParing.php
@@ -1,30 +1,115 @@
 <?php
-//Написать консольный парсер, который принимает на вход урл, валидирует его
-//И получает по заданному урлу ссылки на все картинки. Эти ссылки сохранить в цсв файл.
 
-include_once ('curl.php');
-$Url = $argv[1];
-$data = curl_get($Url);
-$file = 'pars2.csv';
-$images = array();
 
-preg_match_all('/(img|src)=("|\')[^"\'>]+/i', $data, $media);
-unset($data);
-$data = preg_replace('/(img|src)("|\'|="|=\')(.*)/i', "$3", $media[0]);
+require_once 'classes/parsing.php';
+//use classes\parsing;
 
-foreach ($data as $url) {
-    $info = pathinfo($url);
-    if (isset($info['extension'])) {
-        if (($info['extension'] == 'jpg') ||
-            ($info['extension'] == 'jpeg') ||
-            ($info['extension'] == 'gif') ||
-            ($info['extension'] == 'png'))
-            array_push($images, $url);
-    }
+system("clear"); //Очищаем экран перед выводом
+// Шапка программы (там написано contacts)
+echo "             _           _\n";
+echo " ___ ___ ___| |_ ___ ___| |_ ___\n";
+echo "|  _| . |   |  _| .'|  _|  _|_ -|\n";
+echo "|___|___|_|_|_| |__,|___|_| |___|\n";
+echo "Надёжное хранение ваших контактов\n";
+
+switch ($argv[1]) {
+    case 'parse ':
+        if (isset($argv[2])) {
+            $pars = new parsing();
+            $pars->setCurrentUrl($argv[2]);
+            $pars->setInternalLinks();
+            $test = $pars->getInternalLinks();
+            if(count($test)>0){
+                $i = 0;
+                while($test){
+                    $newLink = new parsing();
+                    $newLink->setCurrentUrl($test[$i]);
+                    $newLink->getImageLink();
+                    $pars->addInternalLink($test[$i]);
+                    $pars->setVisitedPages($test[$i]);
+                    $pars->deleteInternalLinks($test[$i]);
+//        echo 'good';break;
+
+                    if(count($pars->getVisitedPages()) > 5){
+                        var_dump($pars->getVisitedPages());
+                        break;
+                    }
+                    $i++;
+                }
+            }else{
+                echo 'Error';
+            }
+        }
+        echo "Ошибка параметра, воспользуйтесь справкой \n";
+        break;
+//    case 'edit':
+//        if (isset($argv[2])) {
+//            $do = new action();
+//            $do ->edit($argv[2]);
+//        }
+//        echo "Ошибка параметра, воспользуйтесь справкой \n";
+//        break;
+    case 'add':
+       echo 'add';
+        break;
+//    case 'delete':
+//////        if (isset($argv[2])) {
+//////            $do = new action();
+//////            $do ->delete($argv[2]);
+//////        }
+//////        echo "Ошибка параметра, воспользуйтесь справкой \n";
+//////        break;
+//////    case 'help':
+//////        $do = new action();
+//////        $do ->help();
+//////        break;
+
+    default:
+        // Если параметр нам не подходит, или его нет, говорим об ошибке
+        echo "Ошибка параметра, воспользуйтесь справкой \n";
+        break;
 }
-foreach ($images as $value) {
-    //echo $value,'<br>';
-    file_put_contents($file, $value."\n", FILE_APPEND | LOCK_EX);
+echo "\n"; // Делаем ещё один перевод строки, что бы отделить вывод программы
 
-}
-?>
+
+
+//$pars = new parsing();
+//$pars->setCurrentUrl($argv[1]);
+//$pars->setParseUrlLimit(500);
+//
+//$pars->setInternalLinks();
+//$test = $pars->getInternalLinks();
+////$test = $pars->mainLink();
+//var_dump($test[0]);
+//
+//
+//if(count($test)>0){
+//    $i = 0;
+//    while($test){
+//        $newLink = new parsing();
+//        $newLink->setCurrentUrl($test[$i]);
+//        $newLink->getImageLink();
+//        $pars->addInternalLink($test[$i]);
+//        $pars->setVisitedPages($test[$i]);
+//        $pars->deleteInternalLinks($test[$i]);
+////        echo 'good';break;
+//
+//        if(count($pars->getVisitedPages()) > 5){
+//            var_dump($pars->getVisitedPages());
+//            break;
+//        }
+//        $i++;
+//    }
+//}else{
+//    echo 'Error';
+//}
+
+
+
+
+/*$result = dns_get_record($argv[1]);
+print_r($result);*/
+
+
+//    echo ($i+1).PHP_EOL;
+
